87c580622d74160d8b6f51dec7237ddf0534414c
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-04-15 16:09:43 -0300

    Adicionado ícone da extensão

--- extensao/content.js
+++ extensao/content.js
@@ -2,3 +2,13 @@
 var firstHref = $("a[href^='http']").eq(0).attr("href");
 
 console.log(firstHref);
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+      console.log(firstHref);
+    }
+  }
+);
