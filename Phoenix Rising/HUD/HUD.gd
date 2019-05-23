60dc38ea244384fa2c8305a683f1bb2c1cd844f1
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-05-23 16:48:55 -0300

    Refatoração de Title Screen

--- Phoenix Rising/HUD/HUD.gd
+++ Phoenix Rising/HUD/HUD.gd
@@ -11,8 +11,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #    pass
-
-func _on_NewGame_pressed():
-    $Title.hide()
-    $Author.hide()
-    $NewGame.hide()
