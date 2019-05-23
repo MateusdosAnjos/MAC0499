bbf9376f1b3de77cd976d8c116e48aac9a21e311
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-05-23 16:08:51 -0300

    Resolvido botao de fullscreen

--- Phoenix Rising/TelaInicial/TitleScreen.gd
+++ Phoenix Rising/TelaInicial/TitleScreen.gd
@@ -15,3 +15,7 @@ func _ready():
 
 func _on_NewGame_pressed():
     get_tree().change_scene("res://Main/Main.tscn")
+
+
+func _on_FullScreen_pressed():
+    OS.window_fullscreen = !OS.window_fullscreen
