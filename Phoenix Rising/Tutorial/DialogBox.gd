f37a90fdc3a29a20453949cd5a44d146ae5d92fb
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-05-25 15:19:20 -0300

    Nova forma de acelerar o tutorial

--- Phoenix Rising/Tutorial/DialogBox.gd
+++ Phoenix Rising/Tutorial/DialogBox.gd
@@ -4,25 +4,25 @@ extends RichTextLabel
 # Variables
 var dialog = [
     "Bem vindo ao Phoenix Rising.",
-    "Este é o tutorial."
+    "Este é o tutorial.",
+    "Agora adicionei algumas mensagens de teste.",
     ]
 var page = 0
 
 # Functions
 func _ready():
-	set_process_input(true)
-	set_bbcode(dialog[page])
-	set_visible_characters(0)
-
-func _input(event):
-	if event is InputEventMouseButton && event.is_pressed():
-		if get_visible_characters() > get_total_character_count():
-			if page < dialog.size()-1:
-				page += 1
-				set_bbcode(dialog[page])
-				set_visible_characters(0)
-		else:
-			set_visible_characters(get_total_character_count())
+    set_process_input(true)
+    set_bbcode(dialog[page])
+    set_visible_characters(0)
 
 func _on_Timer_timeout():
-	set_visible_characters(get_visible_characters()+1)
+    set_visible_characters(get_visible_characters()+1)
+
+func _on_DialogBox_pressed():
+        if get_visible_characters() > get_total_character_count():
+            if page < dialog.size()-1:
+                page += 1
+                set_bbcode(dialog[page])
+                set_visible_characters(0)
+        else:
+            set_visible_characters(get_total_character_count())
