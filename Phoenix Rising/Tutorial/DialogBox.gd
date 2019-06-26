21013212d3893af56bfdb8c68d15c9ffef86e265
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-06-25 23:01:34 -0300

    Adicionado tutorial esconde após o termino das mensagens

--- Phoenix Rising/Tutorial/DialogBox.gd
+++ Phoenix Rising/Tutorial/DialogBox.gd
@@ -8,22 +8,31 @@ var dialog = [
     "Agora adicionei algumas mensagens de teste.",
     ]
 var page = 0
+var maxPages = 2
+
+onready var SkipButton = get_parent().get_node("Skip")
 
 # Functions
 func _ready():
     set_process_input(true)
     set_bbcode(dialog[page])
     set_visible_characters(0)
+    
 
 func _on_Timer_timeout():
     set_visible_characters(get_visible_characters()+1)
+    if page == maxPages and get_visible_characters() > get_total_character_count():
+        SkipButton.text = "Esconder"   
     
 func _on_Skip_pressed():
-        if get_visible_characters() > get_total_character_count():
-            if page < dialog.size()-1:
-                page += 1
-                set_bbcode(dialog[page])
-                set_visible_characters(0)
-        else:
-            set_visible_characters(get_total_character_count())
+        if SkipButton.text == "Esconder":
+            get_parent().hide()
+        else:    
+            if get_visible_characters() > get_total_character_count():
+                if page < dialog.size()-1:
+                    page += 1
+                    set_bbcode(dialog[page])
+                    set_visible_characters(0)
+            else:
+                set_visible_characters(get_total_character_count())
 
--- Phoenix Rising/Tutorial/Tutorial.gd
+++ Phoenix Rising/Tutorial/Tutorial.gd
@@ -6,6 +6,5 @@ func _ready():
 func _on_FullScreen_pressed():
     OS.window_fullscreen = !OS.window_fullscreen
     
-
 func _on_Nivel_1_pressed():
     get_tree().change_scene("res://Nivel1/Nivel1.tscn")
