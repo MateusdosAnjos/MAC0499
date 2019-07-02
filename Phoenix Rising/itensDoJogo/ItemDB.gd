fb2897f3c42d9756e8a125b4b3cb9fe1217678a6
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-07-02 00:57:43 -0300

    Atualizado banco de itens com espaço para caminho do código

--- Phoenix Rising/itensDoJogo/ItemDB.gd
+++ Phoenix Rising/itensDoJogo/ItemDB.gd
@@ -1,23 +1,26 @@
 extends Node2D
 
-
 const ICON_PATH = "res://acessorios/art/"
 const ITEMS = {
     "godot": {
         "icon": ICON_PATH + "icon.png",
-        "help": "Icone da Godot para testes"
+        "help": "Icone da Godot para testes",
+        "codePath": "res://godot.gd"
     },
     "phoenixD": {
         "icon": ICON_PATH + "phoenix_direita.png",
-        "help": "Icone da Phoenix para a direita!!"
+        "help": "Icone da Phoenix para a direita!!",
+        "codePath": "res://phoenixD.gd"
     },
     "phoenixE": {
         "icon": ICON_PATH + "phoenix_esquerda.png",
-        "help": "Icone da Phoenix para a esquerda!!"
+        "help": "Icone da Phoenix para a esquerda!!",
+        "codePath": "res://phoenixE.gd"
     },         
     "error": {
         "icon": ICON_PATH + "enemyFlyingAlt_2.png",
-        "help": "Icone de erro para testes"
+        "help": "Icone de erro para testes",
+        "codePath": "res://error.gd"
        }
 }
 func get_item(item_id):
