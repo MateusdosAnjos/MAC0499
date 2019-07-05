7f59cfb76db9725176341c96159f630faca41aa0
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-07-05 00:40:44 -0300

    Update RunButton.gd

--- Phoenix Rising/ItensDoJogo/RunButton.gd
+++ Phoenix Rising/ItensDoJogo/RunButton.gd
@@ -4,10 +4,6 @@ extends Button
 func _ready():
     pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#    pass
-
 func _on_Run_pressed():
     var items = []
     var actionsPath = []
