7b8e5e529cdf6aed3400eb22f88150fb2ed8c708
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-07-05 17:47:18 -0300

    run button agora carrega comportamentos dos itens e os executa em sequencia

--- Phoenix Rising/ItensDoJogo/RunButton.gd
+++ Phoenix Rising/ItensDoJogo/RunButton.gd
@@ -20,18 +20,22 @@ func _get_items_code_path(items, code_paths):
         code_paths.append(ItemDB.get_item(item)["codePath"])
 
 func _process_input(input, code_paths):
+    var processed_input = input
     for item_code in code_paths:
-        var godotScript = load(item_code)
-        godotScript.new(input)               
+        var input_process_code = load(item_code)
+        var code_node = input_process_code.new()
+        processed_input = code_node.execute(processed_input)      
+    return processed_input                 
     
 func _on_Run_pressed():
     var items = []
     var code_paths = []
     _get_ActionSpaces_item_list(items)  
     _get_items_code_path(items, code_paths)  
-    _process_input(input, code_paths)
-    if (input == output):
-        print("Parabéns voce conseguiu!")  
+    if (_process_input(input, code_paths) == output):
+        print("Parabéns voce conseguiu!")
+    else:
+        print("Ops, algo está errado!")     
     
 func _on_Tutorial_input_output_defined(inp, out):
     input = inp
