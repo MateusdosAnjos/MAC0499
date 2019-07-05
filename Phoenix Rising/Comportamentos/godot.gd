276c4d244fba8382065a17c540708f75000316ba
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-07-05 17:46:46 -0300

    update nos comportamentos dos itens de teste

--- Phoenix Rising/Comportamentos/godot.gd
+++ Phoenix Rising/Comportamentos/godot.gd
@@ -1,12 +1,7 @@
 extends Node
 
-func _init(argument_list):
-    print(argument_list)
-    
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func _init():
     pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#    pass
+func execute(argument_list):
+    return 5
--- Phoenix Rising/Comportamentos/phoenixD.gd
+++ Phoenix Rising/Comportamentos/phoenixD.gd
@@ -1,15 +1,7 @@
 extends Node
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-func _init(argument_list):
-    print("PHOENIXD script loaded")
-    
-# Called when the node enters the scene tree for the first time.
-func _ready():
-    pass # Replace with function body.
+func _init():
+    pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#    pass
+func execute(argument_list):
+    return 5
