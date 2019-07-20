8ec2dd531b4da33985d7d14e045f019906a7ec94
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-07-20 16:24:45 -0300

    Run button consegue acessar os argumentos dos Action Spaces

--- Phoenix Rising/ItensDoJogo/RunButton.gd
+++ Phoenix Rising/ItensDoJogo/RunButton.gd
@@ -6,18 +6,27 @@ var output
 func _ready():
     pass
 
-func _get_ActionSpaces_item_list(list):
+func _get_ActionSpaces_node_list(node_list):
     var regex = RegEx.new()
     regex.compile("ActionSpace*")
     var inventoryNode = (self.owner).get_node('Inventory')
     for node in inventoryNode.get_children():
         if regex.search(node.get_name()) and node.placed_item:
-            list.append(node.placed_item.get_meta("id"))
+            node_list.append(node)
+    return
+            
+func _get_ActionSpaces_item_list(item_list, node_list):
+    for node in node_list:
+            item_list.append(node.placed_item.get_meta("id"))
     return            
 
 func _get_items_code_path(items, code_paths):
     for item in items:
         code_paths.append(ItemDB.get_item(item)["codePath"])
+
+func _get_items_arguments(node_list, arguments_list):
+    for item in node_list:
+        arguments_list.append(item.argument_list)        
 
 func _process_input(input, code_paths):
     var processed_input = input
@@ -28,10 +37,15 @@ func _process_input(input, code_paths):
     return processed_input                 
     
 func _on_Run_pressed():
-    var items = []
+    var node_list = []
+    var item_list = []
     var code_paths = []
-    _get_ActionSpaces_item_list(items)  
-    _get_items_code_path(items, code_paths)  
+    var arguments_list = []
+    _get_ActionSpaces_node_list(node_list)
+    _get_ActionSpaces_item_list(item_list, node_list)  
+    _get_items_code_path(item_list, code_paths)
+    _get_items_arguments(node_list, arguments_list)
+    print(arguments_list)
     if (_process_input(input, code_paths) == output):
         print("Parabéns voce conseguiu!")
     else:
