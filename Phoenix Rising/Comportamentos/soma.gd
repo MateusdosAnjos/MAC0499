7f089ee0f801ac043d02d3e244e75d932b4d3bb5
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-08-12 11:21:45 -0300

    soma.gd com alguns comentários

--- Phoenix Rising/Comportamentos/soma.gd
+++ Phoenix Rising/Comportamentos/soma.gd
@@ -5,16 +5,21 @@ func _init():
 
 func execute(input, arguments, player_answer):
     arguments = arguments.split(", ")
+    
+    #Arguments check
     if (arguments.size() < 2):
         #wrong_arguments_message()
-        return null    
+        return null
+    #Accepting "input" as part of operations        
     if (arguments[0] == 'input'):
         arguments[0] = input
     if (arguments[1] == 'input'):
         arguments[1] = input
+    #Checking types to perform the right "+" operation    
     if (arguments[0].is_valid_integer() and arguments[1].is_valid_integer()):
         return (int(arguments[0]) + int(arguments[1]))
     elif (arguments[0].is_valid_float() and arguments[1].is_valid_float()):
-        return (float(arguments[0]) + float(arguments[1]))    
+        return (float(arguments[0]) + float(arguments[1]))
+    #This one is for Strings       
     else:
         return (arguments[0] + arguments[1])
