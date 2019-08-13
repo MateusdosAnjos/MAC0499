40edf5c9a3e8836f86563f8b3d3b4cb9ef39ba36
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-08-13 20:37:43 -0300

    Trocado o nó do script em DialogBox

--- Phoenix Rising/Tutorial/DialogBox.gd
+++ Phoenix Rising/Tutorial/DialogBox.gd
@@ -1,5 +1,5 @@
 # DialogBox.gd
-extends RichTextLabel
+extends Panel
 
 # Variables
 var dialog = [
@@ -11,34 +11,36 @@ var visual_dialog_nodes = ["InputFrame", "ExpectedOutputFrame", "PlayerOutputFra
 var page = 0
 var max_pages = 2
 
-onready var SkipButton = get_parent().get_node("Skip")
+onready var SkipButton = get_node("Skip")
+onready var TextBox = get_node("TextBox")
 
 # Functions
 func _ready():
     set_process_input(true)
-    set_bbcode(dialog[page])
-    set_visible_characters(0)
+    TextBox.set_bbcode(dialog[page])
+    TextBox.set_visible_characters(0)
     for node_name in visual_dialog_nodes:
-        get_parent().get_node(node_name).hide()
+        get_node(node_name).hide()
 
 func _on_Timer_timeout():
-    set_visible_characters(get_visible_characters()+1)
-    if page == max_pages and get_visible_characters() > get_total_character_count():
+    TextBox.set_visible_characters(TextBox.get_visible_characters()+1)
+    if page == max_pages and TextBox.get_visible_characters() > TextBox.get_total_character_count():
         SkipButton.text = "Esconder"   
     
 func _on_Skip_pressed():
     show_visual()
     if SkipButton.text == "Esconder":
-        get_parent().hide()
+        hide()
         get_tree().paused = false
     else:    
-        if get_visible_characters() > get_total_character_count():
+        if TextBox.get_visible_characters() > TextBox.get_total_character_count():
             if page < dialog.size()-1:
                 page += 1
-                set_bbcode(dialog[page])
-                set_visible_characters(0)
+                TextBox.set_bbcode(dialog[page])
+                TextBox.set_visible_characters(0)
         else:
-            set_visible_characters(get_total_character_count())
+            TextBox.set_visible_characters(TextBox.get_total_character_count())
 
 func show_visual():
-    get_parent().get_node(visual_dialog_nodes[page]).show()
+    get_node(visual_dialog_nodes[page]).show()
+    
