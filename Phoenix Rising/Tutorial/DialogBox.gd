9b12155032f4d44feeb733c21ae32e21d1ed28b6
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-08-13 20:52:40 -0300

    Agora os retângulos piscam!!!

--- Phoenix Rising/Tutorial/DialogBox.gd
+++ Phoenix Rising/Tutorial/DialogBox.gd
@@ -14,14 +14,26 @@ var max_pages = 2
 onready var SkipButton = get_node("Skip")
 onready var TextBox = get_node("TextBox")
 
-# Functions
+# Functions        
 func _ready():
     set_process_input(true)
     TextBox.set_bbcode(dialog[page])
     TextBox.set_visible_characters(0)
     for node_name in visual_dialog_nodes:
         get_node(node_name).hide()
-
+        
+func _process(delta):
+    if page == 0:
+        $InputFrame.play("flashy")
+    if page == 1:    
+        $InputFrame.stop()
+        $ExpectedOutputFrame.play("flashy")
+    if page == 2:
+        $ExpectedOutputFrame.stop()
+        $PlayerOutputFrame.play("flashy")
+    if page == 3:
+        $PlayerOutputFrame.stop()       
+        
 func _on_Timer_timeout():
     TextBox.set_visible_characters(TextBox.get_visible_characters()+1)
     if page == max_pages and TextBox.get_visible_characters() > TextBox.get_total_character_count():
