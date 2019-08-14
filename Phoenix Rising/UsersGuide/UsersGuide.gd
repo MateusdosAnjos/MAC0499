58e5ba9c3d88b94f35f28d821d174bd60dc3b0ff
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-08-14 17:39:45 -0300

    Mostrando área de comandos

--- Phoenix Rising/UsersGuide/UsersGuide.gd
+++ Phoenix Rising/UsersGuide/UsersGuide.gd
@@ -7,11 +7,12 @@ var dialog = [
     'A partir do valor de [color=#00007f][b] ENTRADA [/b][/color] você deve obter o valor de [color=green][b] SAÍDA ESPERADA[/b][/color].\nVocê pode visualizar sua saída em [color=red][b] SAÍDA [/b][/color]',
     '[color=#00007f][b] ENTRADA [/b][/color] está marcada com o retângulo [color=#00007f][b] AZUL[/b][/color].',
     '[color=green][b] SAÍDA ESPERADA [/b][/color] está marcada com o retângulo [color=green][b] VERDE[/b][/color].',    
-    '[color=red][b] SAÍDA [/b][/color] está marcada com o retângulo [color=red][b] VERMELHO[/b][/color].',  
+    '[color=red][b] SAÍDA [/b][/color] está marcada com o retângulo [color=red][b] VERMELHO[/b][/color].',
+    'Para obter a resposta você deve utilizar os comandos disponibilizados na sua [color=black][b] ÁREA DE COMANDOS[/b][/color].\nLembre-se de que nem sempre você precisará utilizar todos eles!',
     ]    
-var visual_dialog_nodes = ["InputFrame", "ExpectedOutputFrame", "PlayerOutputFrame"]    
+var visual_dialog_nodes = ["InputFrame", "ExpectedOutputFrame", "PlayerOutputFrame", "InventoryArrow"]    
 var page = 0
-var max_pages = 4
+var max_pages = 5
 
 onready var SkipButton = get_node("DialogBox/Skip")
 onready var TextBox = get_node("DialogBox/TextBox")
@@ -32,8 +33,6 @@ func _on_Timer_timeout():
 func _on_Skip_pressed():
     if SkipButton.text == "Esconder":
         $DialogBox.hide()
-        $PlayerOutputFrame.stop()
-        $PlayerOutputFrame.set_frame(0)
         get_tree().paused = false
     else:    
         if TextBox.get_visible_characters() >= TextBox.get_total_character_count():
@@ -47,8 +46,9 @@ func _on_Skip_pressed():
 
 func show_visuals():
     if page == 1:
-        for node_name in visual_dialog_nodes:
-            get_node(node_name).show()       
+        get_node(visual_dialog_nodes[0]).show()
+        get_node(visual_dialog_nodes[1]).show()   
+        get_node(visual_dialog_nodes[2]).show()         
     if page == 2:
         $InputFrame.play("flashy")
     if page == 3:
@@ -58,4 +58,9 @@ func show_visuals():
     if page == 4:
         $ExpectedOutputFrame.stop()
         $ExpectedOutputFrame.set_frame(0)
-        $PlayerOutputFrame.play("flashy")     
+        $PlayerOutputFrame.play("flashy")
+    if page == 5:
+        $PlayerOutputFrame.stop()
+        $PlayerOutputFrame.set_frame(0)
+        $InventoryArrow.show()
+        $InventoryArrow.play("flashy")          
