a7f59074a68c8da9432cb10e903c05cb6196a26f
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-08-15 14:35:03 -0300

    Feito um objeto andante e mutável
    Signed-off-by: MateusdosAnjos <mateus.a.anjos@gmail.com>

--- Phoenix Rising/Sprite.gd
+++ Phoenix Rising/Sprite.gd
@@ -1,7 +1,4 @@
-extends Sprite
-
-onready var texture1 = preload("res://Acessorios/art/BluePhoenixLogo.png")
-onready var texture2 = preload("res://Acessorios/art/PhoenixLogo.png")
+extends Label
 
 var i = 0
 
@@ -9,12 +6,9 @@ func _ready():
     set_physics_process(true)
 
 func _physics_process(delta):
-    get_parent().set_offset(get_parent().get_offset() + (500*delta))
-    
+    get_parent().set_offset(get_parent().get_offset() + (60*delta))   
+
 
 func _on_Timer_timeout():
-    if i%2 == 0:
-        self.texture = texture1
-    else:
-        self.texture = texture2
-    i += 1        
+    self.text = str(i)
+    i = i + 1
