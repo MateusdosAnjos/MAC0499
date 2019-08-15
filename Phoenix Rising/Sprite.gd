edd84b76ed5537a4b72fa08032b2bb0a1666a0e1
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-08-15 14:40:44 -0300

    Adicionado novo caminho a ser seguido
    Signed-off-by: MateusdosAnjos <mateus.a.anjos@gmail.com>

--- Phoenix Rising/Sprite.gd
+++ Phoenix Rising/Sprite.gd
@@ -6,7 +6,7 @@ func _ready():
     set_physics_process(true)
 
 func _physics_process(delta):
-    get_parent().set_offset(get_parent().get_offset() + (60*delta))   
+    get_parent().set_offset(get_parent().get_offset() + (180*delta))   
 
 
 func _on_Timer_timeout():
