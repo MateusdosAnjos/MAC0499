b44b46233495f5a2a7f2d6c093279f27e1889282
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-08-15 14:24:35 -0300

    Trocando a textura do sprite via codigo
    Signed-off-by: MateusdosAnjos <mateus.a.anjos@gmail.com>

--- Phoenix Rising/Sprite.gd
+++ Phoenix Rising/Sprite.gd
@@ -1,8 +1,20 @@
 extends Sprite
 
+onready var texture1 = preload("res://Acessorios/art/icon.png")
+onready var texture2 = preload("res://Acessorios/art/PhoenixLogo.png")
+
+var i = 0
+
 func _ready():
     set_physics_process(true)
 
 func _physics_process(delta):
     get_parent().set_offset(get_parent().get_offset() + (500*delta))
     
+
+func _on_Timer_timeout():
+    if i%2 == 0:
+        self.texture = texture1
+    else:
+        self.texture = texture2
+    i += 1        
