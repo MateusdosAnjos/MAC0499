13b7af7c8b939cfde3523f0961bc86af7c24a000
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-08-16 18:08:57 -0300

    Melhorado o MovableActionSpace

---
+++ Phoenix Rising/Sandbox/MovableActionSpace.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+var mouseIn = false
+
+
+func _process(delta):
+    # You need to create a action caleld "click"(Left mouse click) in the project input map.
+    # Você precisa criar uma ação chamada click(botão esquerdo do mouse) no input map do seu projeto
+    if(mouseIn && Input.is_action_pressed("inv_grab")):
+        print("Beleza")
+        set_position(get_viewport().get_mouse_position())
+    pass
+
+
+func _on_Area2D_mouse_entered():
+    print("Oe")
+    mouseIn = true
+
+
+
+func _on_Area2D_mouse_exited():
+    mouseIn = false
