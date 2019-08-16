49bd47b23bd8f3b8f0c8b7eeab34b85837290a49
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-08-16 17:58:03 -0300

    Criado teste para o ActionSpace móvel

--- Phoenix Rising/ActionSpace/Action.gd
+++ Phoenix Rising/ActionSpace/Action.gd
@@ -113,3 +113,33 @@ func show_message(state):
     
 func _on_ArgumentButton_sent_arguments(arguments):
     argument_list = arguments
+
+
+
+
+
+
+
+
+#MOVABLE PART
+
+
+var mouseIn = false
+
+
+func _process(delta):
+    # You need to create a action caleld "click"(Left mouse click) in the project input map.
+    # Você precisa criar uma ação chamada click(botão esquerdo do mouse) no input map do seu projeto
+    if(mouseIn && Input.is_action_pressed("inv_grab")):
+        print("Beleza")
+        set_position(get_viewport().get_mouse_position())
+    pass
+
+
+func _on_Area2D_mouse_entered():
+    mouseIn = true
+
+
+
+func _on_Area2D_mouse_exited():
+    mouseIn = false
