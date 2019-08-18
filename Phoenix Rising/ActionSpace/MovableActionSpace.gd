4f624bdd572d3bb7d1632fe85464a22c5466c733
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-08-18 13:04:29 -0300

    Criado código e documentação para enumeração dos action spaces

--- Phoenix Rising/ActionSpace/MovableActionSpace.gd
+++ Phoenix Rising/ActionSpace/MovableActionSpace.gd
@@ -33,7 +33,21 @@ func _on_Area2D_mouse_exited():
     mouseIn = false
 
 func _on_InputArea_area_shape_entered(area_id, area, area_shape, self_shape):
+    #Changes the texture to the green one
     $InputArea/Sprite.texture = connected_texture
+    #StartInputArea is the action with number 0 and has no atribute "text" 
+    if area.name != "StartInputArea":
+        #'area' holds the area2D of the MovableActionSpace that is fixed (not
+        #from the one we are moving in the game).
+        #First gets the parent of 'area', then gets the "ActionNumber" node
+        #and acess the 'text' field(the action number), transforming the string
+        #into an int to perform operations
+        var action_number = int(area.get_parent().get_node("ActionNumber").text)
+        #Increase the action number by 1 (that's why is the 'next'). Now we have
+        #the action number of the node we are moving in the game
+        var next_action_number = action_number + 1
+        #Place the correct number for the moving ActionSpace in game
+        $ActionNumber.text = str(next_action_number)
 
 func _on_InputArea_area_shape_exited(area_id, area, area_shape, self_shape):
     $InputArea/Sprite.texture = not_connected_texture
