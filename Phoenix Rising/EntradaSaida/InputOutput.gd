1bf7e96f534eb8d9e16e0a5011ae004477099458
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-08-18 10:09:37 -0300

    renomeado funções das entradas iniciais e finais

--- Phoenix Rising/EntradaSaida/InputOutput.gd
+++ Phoenix Rising/EntradaSaida/InputOutput.gd
@@ -1,25 +1,25 @@
 extends Control
 
-signal input_output_entered()
-signal output_input_entered()
-signal input_output_exited()
-signal output_input_exited()
+signal start_input_entered()
+signal finish_input_entered()
+signal start_input_exited()
+signal finish_input_exited()
 
 onready var connected_texture = preload("res://Acessorios/art/input_output_with_connection.png")
 onready var not_connected_texture = preload("res://Acessorios/art/input_output_no_connection.png")
 
-func _on_InputOutputArea_area_entered(area):
-    $InputOutputArea/Sprite.texture = connected_texture
-    emit_signal("input_output_entered")
+func _on_StartInputArea_area_entered(area):
+    $StartInputArea/Sprite.texture = connected_texture
+    emit_signal("start_input_entered")
     
-func _on_InputOutputArea_area_exited(area):
-    $InputOutputArea/Sprite.texture = not_connected_texture
-    emit_signal("input_output_exited")
+func _on_StartInputArea_area_exited(area):
+    $StartInputArea/Sprite.texture = not_connected_texture
+    emit_signal("start_input_exited")
 
-func _on_OutputInputArea_area_entered(area):
-    $OutputInputArea/Sprite.texture = connected_texture
-    emit_signal("output_input_entered")
+func _on_FinishInputArea_area_entered(area):
+    $FinishInputArea/Sprite.texture = connected_texture
+    emit_signal("finish_input_entered")
 
-func _on_OutputInputArea_area_exited(area):
-    $OutputInputArea/Sprite.texture = not_connected_texture
-    emit_signal("output_input_exited")
+func _on_FinishInputArea_area_exited(area):
+    $FinishInputArea/Sprite.texture = not_connected_texture
+    emit_signal("finish_input_exited")
