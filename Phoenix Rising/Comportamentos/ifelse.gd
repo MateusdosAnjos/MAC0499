714f812fd7249d351ed5e8095838761b40895e52
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-08-21 17:08:25 -0300

    IfElse funcionando para um caso BEM simples

--- Phoenix Rising/Comportamentos/ifelse.gd
+++ Phoenix Rising/Comportamentos/ifelse.gd
@@ -7,15 +7,15 @@ func execute(input, arguments, player_answer):
     arguments = arguments.split(" ", true, 1)
     match arguments[0]:
         '<':
-            return input < arguments[1]   
+            return [0, input < int(arguments[1])] 
         '<=':
-            return input <= arguments[1]
+            return [0, input <= arguments[1]]
         '>':
-            return input > arguments[1]
+            return [0, input > int(arguments[1])]
         '>=':
-            return input >= arguments[1]
+            return [0, input >= arguments[1]]
         '==':
-            return input == arguments[1]
+            return [0, input == arguments[1]]
         _:
             #wrong_arguments_message()
             print("Nenhuma das anteriores")
