64b4c73d39623389ca66ff359e35ba1aaebff8ca
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-08-22 13:50:44 -0300

    Comentado codigo de Action.gd
    Signed-off-by: MateusdosAnjos <mateus.a.anjos@gmail.com>

--- Phoenix Rising/ActionSpace/Action.gd
+++ Phoenix Rising/ActionSpace/Action.gd
@@ -1,15 +1,22 @@
 extends Control
 
+#Keeps the item placed on ActionSpace
 var placed_item = null
+#Used to create a metric for item positioning
 var grid = {}
+#Size of each grid cell
 var cell_size = 2
+#Size of the grid
 var grid_width = 0
 var grid_height = 0
+#The grid center, used to position the item
 var grid_center = []
+#Argument list to be passed when executing the item code (see RunButton.gd)
 var argument_list = []
-
+#Used to know when ActionSpace already has an item placed
 onready var exist_item = false
 
+#_ready initializes the grid
 func _ready():
     var s = get_grid_size(self)
     grid_width = s.x
@@ -22,6 +29,8 @@ func _ready():
         for y in range (grid_height):
             grid[x][y] = false
             
+#This function inserts the item on Action Space if it is possible
+#returns true when it was possible to insert and false when it's not
 func insert_item(item):
     var item_size = get_grid_size(item)
     var g_pos = {}
@@ -41,6 +50,9 @@ func insert_item(item):
     else:
         return false        
 
+#Receives a position(pos) and tries to grab the item under it
+#(Each Action Space holds a maximum of 1 item, that's why it's THE
+#item)
 func grab_item(pos):
     var item = get_item_under_pos(pos)
     if item == null:
@@ -53,7 +65,9 @@ func grab_item(pos):
     exist_item = false
     show_message(true)
     return item
-        
+
+#Given a position (pos) returns the item under it or null when
+#there is no item under it.
 func get_item_under_pos(pos):
     if placed_item == null:
         return null
@@ -61,11 +75,17 @@ func get_item_under_pos(pos):
         return placed_item
     return null       
 
+#Receives a (x, y) grid position and sets to
+#state every position on the rectangle (x, x+w, y, y+h)
 func set_grid_space (x, y, w, h, state):
         for i in range (x, x + w):
             for j in range (y, y + h):
                 grid[i][j] = state
-                   
+
+#Receives a rectangle with edges (x, y, x+w, y+h)
+#and checks if there is space on the grid to position
+#this rectangle
+#Returns true if there is grid space and false if there is not
 func is_grid_space_available(x, y, w, h):
     if x < 0 or y < 0:
         return false
@@ -76,21 +96,27 @@ func is_grid_space_available(x, y, w, h):
             if grid[i][j]:
                 return false
     return true
-                                
+
+#Receives a position (pos) and convert it to the
+#grid coordenation system               
 func pos_to_grid_coord(pos):
     var local_pos = pos - rect_global_position
     var results = {}
     results.x = int(local_pos.x / cell_size)
     results.y = int(local_pos.y / cell_size)
     return results
-                         
+
+#Receives an item and return it's size (width and height) using
+#the grid coordenation system             
 func get_grid_size(item):
     var results = {}
     var s = item.rect_size
     results.x = clamp(int(s.x / cell_size), 1, 500)    
     results.y = clamp(int(s.y / cell_size), 1, 500)
     return results
-    
+
+#Receives an item and place it in the first available
+#spot of the grid
 func insert_item_at_first_available_spot(item):
     for y in range (grid_height):
         for x in range (grid_width):
@@ -100,6 +126,8 @@ func insert_item_at_first_available_spot(item):
                     return true
     return false                        
 
+#Shows the "HowToUse" message (Arraste o item aqui)
+#based on the boolean value 'state'
 func show_message(state):
     var message = get_node("HowToUse")
     if state:
@@ -107,6 +135,8 @@ func show_message(state):
     else:
         message.hide()
     return
-    
+
+#The signal passed when its time to fill argument_list
+#variable
 func _on_ArgumentButton_sent_arguments(arguments):
     argument_list = arguments
