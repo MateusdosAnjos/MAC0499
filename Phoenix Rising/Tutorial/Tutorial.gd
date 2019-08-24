f8585ab742b313b1891ab578ca61d7e0f2e77407
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-08-24 16:51:50 -0300

    Modificado nome da pasta Nivel1 para Level1
    Signed-off-by: MateusdosAnjos <mateus.a.anjos@gmail.com>

--- Phoenix Rising/Tutorial/Tutorial.gd
+++ Phoenix Rising/Tutorial/Tutorial.gd
@@ -14,6 +14,6 @@ func _ready():
 
 func _on_FullScreen_pressed():
     OS.window_fullscreen = !OS.window_fullscreen
-    
-func _on_Nivel_1_pressed():
-    get_tree().change_scene("res://Nivel1/Nivel1.tscn")            
+        
+func _on_Level1_pressed():
+    get_tree().change_scene("res://Level1/Level1.tscn")
