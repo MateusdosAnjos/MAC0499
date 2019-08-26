f35bbf1823693945967c358f3e81c812e32ff4e9
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-08-25 21:11:25 -0300

    Comportamento correto do comando If/Else com strings

--- Phoenix Rising/Behaviors/if_else.gd
+++ Phoenix Rising/Behaviors/if_else.gd
@@ -4,18 +4,34 @@ func _init():
     pass
 
 func string_comparision_routine(input, arguments):
-    print(arguments[1].casecmp_to("'Beleza'"))
+    var word_length = arguments[1].length()
+    var word = arguments[1].substr(1, word_length - 2)
     match arguments[0]:
         '<':
-            return [input, input < str(arguments[1])] 
+            if (input.nocasecmp_to(word) == -1):
+                return [input, true]
+            else:
+                return [input, false]
         '<=':
-            return [input, input <= str(arguments[1])]
+            if (input.nocasecmp_to(word) == -1 or input.nocasecmp_to(word) == 0):
+                return [input, true]
+            else:
+                return [input, false]
         '>':
-            return [input, input > str(arguments[1])]
+            if (input.nocasecmp_to(word) == 1):
+                return [input, true]
+            else:
+                return [input, false]
         '>=':
-            return [input, input >= str(arguments[1])]
+            if (input.nocasecmp_to(word) == 1 or input.nocasecmp_to(word) == 0):
+                return [input, true]
+            else:
+                return [input, false]
         '==':
-            return [input, input == str(arguments[1])]
+            if (input.nocasecmp_to(word) == 0):
+                return [input, true]
+            else:
+                return [input, false]
         _:
             #wrong_arguments_message()
             print("Nenhuma das anteriores")
@@ -39,6 +55,6 @@ func number_comparision_routine(input, arguments):
 func execute(input, arguments, player_answer):
     arguments = arguments.split(" ", true, 1)
     if arguments[1][0] == '"' or arguments[1][0] == "'":
-        string_comparision_routine(input, arguments)
+        return string_comparision_routine(input, arguments)
     else:
        return number_comparision_routine(input, arguments)
