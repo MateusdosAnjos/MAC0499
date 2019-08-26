cb71118fec3e23e51b0a740625e2f45ffd197385
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-08-26 12:17:13 -0300

    Adicionado comentários explicativos em ColoredFrames.gd

--- Phoenix Rising/GenericGameScenes/ColoredFrames.gd
+++ Phoenix Rising/GenericGameScenes/ColoredFrames.gd
@@ -1,8 +1,9 @@
 extends Control
 
+#Variable used to control the playing time of the animation 
 var times_up = false
 
-#Makes the Animated sprite with name = node_name flashes for the amount of 'seconds' seconds
+#Makes the Animated sprite named 'node_name' flashes for the amount of 'seconds' seconds
 #0 is the value for playing it until _stop_flashy is called
 func _frame_flashy(node_name, seconds):
     var AnimatedSpriteName = get_node(node_name) 
@@ -17,12 +18,15 @@ func _frame_flashy(node_name, seconds):
         AnimatedSpriteName.set_frame(0)
         AnimatedSpriteName.stop()
 
+#Stops the animation "flashy" in the node named 'node_name'
 func _stop_flashy(node_name):
     get_node(node_name).set_frame(0)
     get_node(node_name).stop()
-    
+
+#Signal from UsersGuide  
 func _on_UsersGuide_frame_flashy(node_name, seconds):
     _frame_flashy(node_name, seconds)
 
+#Signal from UsersGuide
 func _on_UsersGuide_stop_flashy(node_name):
     _stop_flashy(node_name)
