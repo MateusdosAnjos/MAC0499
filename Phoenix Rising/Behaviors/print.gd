a51bbaa0ab4b5cfc9966893ee9c38a1696075bf4
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-08-27 20:14:09 -0300

    print exige que o argumento esteja entre "" ou ''

--- Phoenix Rising/Behaviors/print.gd
+++ Phoenix Rising/Behaviors/print.gd
@@ -3,10 +3,20 @@ extends Node
 func _init():
     pass
 
+#Checks if the argument starts and end with " " or ' '
+func _argument_check(arguments, word_length):
+    if ((arguments[0] == '"' and arguments[word_length-1] == '"') or (arguments[0] == "'" and arguments[word_length-1] == "'")):
+        return true
+
 func execute(input, arguments, player_answer):
     if (not arguments.empty()):
         if (arguments != 'input'):
-            player_answer.append(arguments)
+            var word_length = arguments.length()
+            if (_argument_check(arguments, word_length)):
+                var word = arguments.substr(1, word_length - 2)
+                player_answer.append(word)
+            else:
+                print("Uso errado da função print!")
         else:
             player_answer.append(input)   
     return [input, true]
