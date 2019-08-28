37add8c003baa0acf22651b98d4deba6e04c87cd
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-08-28 16:49:36 -0300

    Popup personalizado para cada item que deu erro

--- Phoenix Rising/Behaviors/print.gd
+++ Phoenix Rising/Behaviors/print.gd
@@ -13,7 +13,7 @@ func execute(input, arguments, player_answer):
                 var word = arguments.substr(1, word_length - 2)
                 player_answer.append(word)
             else:
-                $ErrorMessages.show_error_message()
+                $ErrorMessages.show_error_message("print")
         else:
             player_answer.append(input)   
     return [input, true]
--- Phoenix Rising/RunEnvironment/ErrorMessages.gd
+++ Phoenix Rising/RunEnvironment/ErrorMessages.gd
@@ -1,5 +1,6 @@
 extends Popup
 
-func show_error_message():
-    $Message.set_text("Vem papai")
+func show_error_message(item):
+    var teste = ItemDB.get_item(item)['help']
+    $Message.set_text(teste)
     self.popup()
