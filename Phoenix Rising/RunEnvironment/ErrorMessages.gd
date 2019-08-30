f7f5a44ea1e80fc386959dfd23439e68e5f14de5
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-08-30 14:10:16 -0300

    Mensagem de erro agora trava o jogo até que o botao "Entendido" seja pressionado

--- Phoenix Rising/RunEnvironment/ErrorMessages.gd
+++ Phoenix Rising/RunEnvironment/ErrorMessages.gd
@@ -1,10 +1,13 @@
-extends Popup
+extends Panel
 
 func show_error_message(item, action_number):
     var teste = ItemDB.get_item(item)['help']
     $Message.set_text("Problemas com o comando " + (action_number) + ": ")
     $Message.add_text(teste)
-    self.popup()
-
+    $Message.add_text("\nClique em 'Entendido!' para continuar")
+    self.show()
+    get_tree().paused = true
+    
 func _on_Understood_pressed():
     self.hide()
+    get_tree().paused = false
