f3f57ec8ac5dfdedac531d54bb839cf501c948ef
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-09-02 16:18:25 -0300

    capturado erros em multi.gd

--- Phoenix Rising/Behaviors/multi.gd
+++ Phoenix Rising/Behaviors/multi.gd
@@ -1,20 +1,27 @@
 extends Node
 
+func _split_arguments(arguments):
+    var values = arguments.split(", ", false, 1)
+    if (values.size() < 2):
+        return null
+    else:
+        return values
+
 func execute(input, arguments, player_answer, action_number):
-    arguments = arguments.split(", ")
-    #Arguments check
-    if (arguments.size() < 2):
-        $ErrorMessages.show_error_message("multi", action_number)
-        return [input, true]   
-    if (arguments[0] == 'input'):
-        arguments[0] = input
-    if (arguments[1] == 'input'):
-        arguments[1] = input
-    #Checking types to perform the right "*" operation            
-    if (arguments[0].is_valid_integer() and arguments[1].is_valid_integer()):
-        return [(int(arguments[0]) * int(arguments[1])), true]
-    elif (arguments[0].is_valid_float() and arguments[1].is_valid_float()):
-        return [(float(arguments[0]) * float(arguments[1])), true]      
+    var values = _split_arguments(arguments)
+    if (values != null):
+        if (values[0] == 'input'):
+            values[0] = input
+        if (values[1] == 'input'):
+            values[1] = input
+        #Checking types to perform the right "*" operation            
+        if (values[0].is_valid_integer() and values[1].is_valid_integer()):
+            return [(int(values[0]) * int(values[1])), true]
+        elif (values[0].is_valid_float() and values[1].is_valid_float()):
+            return [(float(values[0]) * float(values[1])), true]      
+        else:
+            $ErrorMessages.show_error_message("multi", action_number)
+            return [input, true]
     else:
         $ErrorMessages.show_error_message("multi", action_number)
         return [input, true]
