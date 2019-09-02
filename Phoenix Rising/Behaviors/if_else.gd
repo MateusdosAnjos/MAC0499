4eca99f0a2f62594d182f4b7f736dc1798624866
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-09-02 15:24:16 -0300

    criado comentário explicativo para script if_else.gd

--- Phoenix Rising/Behaviors/if_else.gd
+++ Phoenix Rising/Behaviors/if_else.gd
@@ -1,6 +1,7 @@
 extends Node
 
-func string_comparision_routine(input, arguments, action_number):
+#Makes the lexicographic comparison between Strings
+func _string_comparision_routine(input, arguments, action_number):
     var word_length = arguments[1].length()
     var word = arguments[1].substr(1, word_length - 2)
     match arguments[0]:
@@ -32,8 +33,9 @@ func string_comparision_routine(input, arguments, action_number):
         _:
             $ErrorMessages.show_error_message("if/else", action_number)
             return [input, false]
-            
-func number_comparision_routine(input, arguments, number_type, action_number):
+
+#Makes the number comparision based on number_type           
+func _number_comparision_routine(input, arguments, number_type, action_number):
     var compare_number = null
     if (number_type == TYPE_INT):
         compare_number = int(arguments[1])
@@ -89,11 +91,11 @@ func execute(input, arguments, player_answer, action_number):
     var key_words = _argument_check(input, arguments)
     if (key_words != null):
         if (typeof(input) == TYPE_STRING):
-            return string_comparision_routine(input, key_words, action_number)
+            return _string_comparision_routine(input, key_words, action_number)
         elif (typeof(input) == TYPE_INT):
-           return number_comparision_routine(input, key_words, TYPE_INT, action_number)
+           return _number_comparision_routine(input, key_words, TYPE_INT, action_number)
         elif (typeof(input) == TYPE_REAL):
-           return number_comparision_routine(input, key_words, TYPE_REAL, action_number)
+           return _number_comparision_routine(input, key_words, TYPE_REAL, action_number)
     else:
         $ErrorMessages.show_error_message("if/else", action_number)
         return [input, false]
