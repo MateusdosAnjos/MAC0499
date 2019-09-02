dc80e1ce38ad57f8c019d50347ece7fc77800192
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-09-02 15:15:58 -0300

    captura de erros em if/else coberta

--- Phoenix Rising/Behaviors/if_else.gd
+++ Phoenix Rising/Behaviors/if_else.gd
@@ -1,6 +1,6 @@
 extends Node
 
-func string_comparision_routine(input, arguments):
+func string_comparision_routine(input, arguments, action_number):
     var word_length = arguments[1].length()
     var word = arguments[1].substr(1, word_length - 2)
     match arguments[0]:
@@ -30,10 +30,10 @@ func string_comparision_routine(input, arguments):
             else:
                 return [input, false]
         _:
-            #wrong_arguments_message()
-            print("Nenhuma das anteriores")
+            $ErrorMessages.show_error_message("if/else", action_number)
+            return [input, false]
             
-func number_comparision_routine(input, arguments, number_type):
+func number_comparision_routine(input, arguments, number_type, action_number):
     var compare_number = null
     if (number_type == TYPE_INT):
         compare_number = int(arguments[1])
@@ -51,8 +51,8 @@ func number_comparision_routine(input, arguments, number_type):
         '==':
             return [input, input == compare_number]
         _:
-            #wrong_arguments_message()
-            print("Nenhuma das anteriores")
+            $ErrorMessages.show_error_message("if/else", action_number)
+            return [input, false]
 
 #Tries to split the arguments in 2. Returns the splited arguments in
 #'key_word' and null if not successful
@@ -89,11 +89,11 @@ func execute(input, arguments, player_answer, action_number):
     var key_words = _argument_check(input, arguments)
     if (key_words != null):
         if (typeof(input) == TYPE_STRING):
-            return string_comparision_routine(input, key_words)
+            return string_comparision_routine(input, key_words, action_number)
         elif (typeof(input) == TYPE_INT):
-           return number_comparision_routine(input, key_words, TYPE_INT)
+           return number_comparision_routine(input, key_words, TYPE_INT, action_number)
         elif (typeof(input) == TYPE_REAL):
-           return number_comparision_routine(input, key_words, TYPE_REAL)
+           return number_comparision_routine(input, key_words, TYPE_REAL, action_number)
     else:
         $ErrorMessages.show_error_message("if/else", action_number)
         return [input, false]
