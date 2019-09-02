7f32fa34f3cee4321b45a4da88b1572f3a2c61a2
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-09-02 11:50:24 -0300

    Mensagem de erro em If/else é exibida caso passe apenas 1 argumento

--- Phoenix Rising/Behaviors/if_else.gd
+++ Phoenix Rising/Behaviors/if_else.gd
@@ -49,13 +49,30 @@ func number_comparision_routine(input, arguments):
             #wrong_arguments_message()
             print("Nenhuma das anteriores")
 
+#Tries to split the arguments in 2. Returns the splited arguments in
+#'key_word' and null if not successful
+func _split_arguments(arguments):
+    var key_words = arguments.split(" ", false, 1)
+    if (key_words.size() < 2):
+        return null
+    else:
+        return key_words
+
+#Checks if the arguments are correctly formated. Returns null if it's
+#not correctly formated
+func _argument_check(arguments):
+    if (arguments.empty()):
+        return null
+    var key_words = _split_arguments(arguments)        
+    return key_words
+        
 func execute(input, arguments, player_answer, action_number):
-    if (not arguments.empty()):
-        arguments = arguments.split(" ", true, 1)
-        if (typeof(input) == TYPE_STRING) and (arguments[1][0] == '"' or arguments[1][0] == "'"):
-            return string_comparision_routine(input, arguments)
+    var key_words = _argument_check(arguments)
+    if (key_words != null):
+        if (typeof(input) == TYPE_STRING) and (key_words[1][0] == '"' or key_words[1][0] == "'"):
+            return string_comparision_routine(input, key_words)
         elif (typeof(input) == TYPE_INT):
-           return number_comparision_routine(input, arguments)
+           return number_comparision_routine(input, key_words)
     else:
         $ErrorMessages.show_error_message("if/else", action_number)
         return [input, false]
