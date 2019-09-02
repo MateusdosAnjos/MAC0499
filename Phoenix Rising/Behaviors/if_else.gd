8003a84be8416247a011f6ca51fe5a5407803ed8
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-09-02 16:03:20 -0300

    padronizando o valor devolvido (padrao true para filhos da direita)

--- Phoenix Rising/Behaviors/if_else.gd
+++ Phoenix Rising/Behaviors/if_else.gd
@@ -32,7 +32,7 @@ func _string_comparision_routine(input, arguments, action_number):
                 return [input, false]
         _:
             $ErrorMessages.show_error_message("if/else", action_number)
-            return [input, false]
+            return [input, true]
 
 #Makes the number comparision based on number_type           
 func _number_comparision_routine(input, arguments, number_type, action_number):
@@ -54,7 +54,7 @@ func _number_comparision_routine(input, arguments, number_type, action_number):
             return [input, input == compare_number]
         _:
             $ErrorMessages.show_error_message("if/else", action_number)
-            return [input, false]
+            return [input, true]
 
 #Tries to split the arguments in 2. Returns the splited arguments in
 #'key_word' and null if not successful
@@ -98,4 +98,4 @@ func execute(input, arguments, player_answer, action_number):
            return _number_comparision_routine(input, key_words, TYPE_REAL, action_number)
     else:
         $ErrorMessages.show_error_message("if/else", action_number)
-        return [input, false]
+        return [input, true]
