510391042b3f96dc8fdfb71234c73a8d327bc105
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-09-03 16:11:07 -0300

    Adicionado Sprites de Output

--- Phoenix Rising/InputOutput/InputOutput.gd
+++ Phoenix Rising/InputOutput/InputOutput.gd
@@ -3,27 +3,29 @@ extends Control
 signal start_input_entered(area)
 signal start_input_exited()
 
-onready var connected_texture = preload("res://Accessories/art/input_with_connection.png")
-onready var not_connected_texture = preload("res://Accessories/art/input_no_connection.png")
+onready var input_connected_texture = preload("res://Accessories/art/input_with_connection.png")
+onready var input_not_connected_texture = preload("res://Accessories/art/input_no_connection.png")
+onready var output_connected_texture = preload("res://Accessories/art/output_with_connection.png")
+onready var output_not_connected_texture = preload("res://Accessories/art/output_no_connection.png")
 
 var input_connected = false
 var output_connected = false
 
 func _on_StartInputArea_area_entered(area):
-    $StartInputArea/Sprite.texture = connected_texture
+    $StartInputArea/Sprite.texture = input_connected_texture
     input_connected = true
     emit_signal("start_input_entered", area)
     
 func _on_StartInputArea_area_exited(area):
-    $StartInputArea/Sprite.texture = not_connected_texture
+    $StartInputArea/Sprite.texture = input_not_connected_texture
     input_connected = false
     emit_signal("start_input_exited")
 
 func _on_FinishInputArea_area_entered(area):
-    $FinishInputArea/Sprite.texture = connected_texture
+    $FinishInputArea/Sprite.texture = output_connected_texture
     if area.get_parent().get_node("ActionNumber").text != "0":
         output_connected = true
     
 func _on_FinishInputArea_area_exited(area):
-    $FinishInputArea/Sprite.texture = not_connected_texture
+    $FinishInputArea/Sprite.texture = output_not_connected_texture
     output_connected = false
