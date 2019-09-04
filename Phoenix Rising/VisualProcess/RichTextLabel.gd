aec400b0d8dd2495dacd0a433be2b4c265b2a84f
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-09-04 16:34:32 -0300

    Texto segue um caminho

---
+++ Phoenix Rising/VisualProcess/RichTextLabel.gd
@@ -0,0 +1,7 @@
+extends RichTextLabel
+
+func _ready():
+    set_physics_process(true)
+
+func _physics_process(delta):
+    get_parent().set_offset(get_parent().get_offset() + (180*delta))
---
+++ Phoenix Rising/VisualProcess/VisualProcess.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+func _ready():
+    var curve = Curve2D.new()
+    curve.add_point(Vector2(120, 70))
+    curve.add_point(Vector2(280, 140))
+    curve.add_point(Vector2(480, 20))
+    $Path.set_curve(curve)
