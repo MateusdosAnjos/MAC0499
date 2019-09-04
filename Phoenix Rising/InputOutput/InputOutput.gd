3cb2f7e26e4b7128af49414f6bba5176f1615932
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-09-04 17:20:41 -0300

    Visual process se inicia do "input" inicial

--- Phoenix Rising/InputOutput/InputOutput.gd
+++ Phoenix Rising/InputOutput/InputOutput.gd
@@ -2,6 +2,7 @@ extends Control
 
 signal start_input_entered(area)
 signal start_input_exited()
+signal start_input_position(pos)
 
 onready var input_connected_texture = preload("res://Accessories/art/input_with_connection.png")
 onready var input_not_connected_texture = preload("res://Accessories/art/input_no_connection.png")
@@ -10,6 +11,9 @@ onready var output_not_connected_texture = preload("res://Accessories/art/output
 
 var input_connected = false
 var output_connected = false
+
+func _ready():
+    emit_signal("start_input_position", $StartInputArea.global_position)
 
 func _on_StartInputArea_area_entered(area):
     $StartInputArea/Sprite.texture = input_connected_texture
--- Phoenix Rising/VisualProcess/VisualProcess.gd
+++ Phoenix Rising/VisualProcess/VisualProcess.gd
@@ -1,7 +1,15 @@
 extends Node2D
 
-func _ready():
+var curve_points = []
+
+func _on_InputOutput_start_input_position(pos):
+    curve_points.append(Vector2(pos[0], pos[1]))
+    _create_curve()
+
+func _create_curve():
     var curve = Curve2D.new()
+    for point in curve_points:
+        curve.add_point(point)
     curve.add_point(Vector2(120, 70))
     curve.add_point(Vector2(280, 140))
     curve.add_point(Vector2(480, 20))
