21ef2b641b7873aa9d42138540c96c8d5fdc3446
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-09-04 16:43:51 -0300

    Instanciado Visual Process em tutorial

--- Phoenix Rising/VisualProcess/RichTextLabel.gd
+++ Phoenix Rising/VisualProcess/RichTextLabel.gd
@@ -4,4 +4,4 @@ func _ready():
     set_physics_process(true)
 
 func _physics_process(delta):
-    get_parent().set_offset(get_parent().get_offset() + (180*delta))
+    get_parent().set_offset(get_parent().get_offset() + (80*delta))
