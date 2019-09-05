5de864da395d026b3db8b8c69292f411461549ef
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-09-04 22:50:44 -0300

    primeira tentativa em modificar o sprite usando o tempo

--- Phoenix Rising/Sandbox/PathFollow.gd
+++ Phoenix Rising/Sandbox/PathFollow.gd
@@ -1,15 +1,18 @@
 extends Node2D
 
+signal distances_set(distances)
+
 func _ready():
     var curve = Curve2D.new()
     var distances = []
-    var points = [Vector2(100, 120), Vector2(250, 200), Vector2(350, 280)]
+    var points = [Vector2(100, 120), Vector2(250, 200), Vector2(350, 280), Vector2(820, 610), Vector2(1115, 430)]
     _calculate_distance(points, distances)
+    emit_signal("distances_set", distances)
     for point in points:
         curve.add_point(point)
     $Path2D.set_curve(curve)
 
 func _calculate_distance(points, distance):
     for j in range (len(points)-1):
-        distance.append( sqrt(((points[j][0] - points[j+1][0]) * (points[j][0] - points[j+1][0])) + ((points[j][1] - points[j+1][1]) * (points[j][1] - points[j+1][1]))))
+        distance.append(int(sqrt(((points[j][0] - points[j+1][0]) * (points[j][0] - points[j+1][0])) + ((points[j][1] - points[j+1][1]) * (points[j][1] - points[j+1][1])))))
     print(distance)
--- Phoenix Rising/Sandbox/Sprite.gd
+++ Phoenix Rising/Sandbox/Sprite.gd
@@ -1,13 +1,21 @@
 extends Label
 
 var i = 0
+var speed = 80
+var times = []
 
 func _ready():
     set_physics_process(true)
 
 func _physics_process(delta):
-    get_parent().set_offset(get_parent().get_offset() + (80*delta))   
+    get_parent().set_offset(get_parent().get_offset() + (speed*delta))   
 
 func _on_Timer_timeout():
     self.text = str(i)
+    get_parent().get_parent().get_parent().get_node("Timer").start(times[i])
     i = i + 1
+
+func _on_Node2D_distances_set(distances):
+    for dist in distances:
+        times.append(dist/speed)
+    print(times)
