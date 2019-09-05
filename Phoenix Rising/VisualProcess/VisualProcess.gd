922e295048c52908bcd8fb60fc8a427704105234
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-09-04 22:16:41 -0300

    Visual input começa escondido

--- Phoenix Rising/VisualProcess/VisualProcess.gd
+++ Phoenix Rising/VisualProcess/VisualProcess.gd
@@ -4,6 +4,9 @@ signal path_compleated()
 
 var starting_pos = null
 
+func _ready():
+    $Path.hide()
+    
 func _on_InputOutput_start_input_position(pos):
     starting_pos = (Vector2(pos[0], pos[1]-50))
 
@@ -20,4 +23,5 @@ func _on_RunEnvironment_visual_process_path_points(path_points):
         curve.add_point(point)
         
     $Path.set_curve(curve)
+    $Path.show()
     emit_signal("path_compleated")
