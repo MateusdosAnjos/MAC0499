70665501fda1f6cb1fb3f39d7d1cb0668f17bc06
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-09-06 14:56:58 -0300

    Sincronização do input inicial com visual process

--- Phoenix Rising/VisualProcess/VisualProcess.gd
+++ Phoenix Rising/VisualProcess/VisualProcess.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var starting_pos = null
 var visual_inputs = []
+var start_input = ''
 var curve = Curve2D.new()
 
 onready var ValueNode = get_node("Path/PathFollow2D/Value")
@@ -11,12 +12,10 @@ func _ready():
     
 func _on_InputOutput_start_input_position(pos):
     starting_pos = (Vector2(pos[0], pos[1]-50))
-    visual_inputs.append("Nada")
+    start_input = "Oi"
 
 func _on_RunEnvironment_visual_process_arguments(path_points, intermediate_inputs):
-    print(visual_inputs)
-    for inputs in intermediate_inputs:
-        visual_inputs.append(inputs)
+    visual_inputs = intermediate_inputs
     curve.clear_points()
     $Path.set_curve(curve)
     var curve_points = []
@@ -30,11 +29,11 @@ func _on_RunEnvironment_visual_process_arguments(path_points, intermediate_input
         curve.add_point(point)
 
 func _on_MovableActionSpace_change_area_entered(action_number):
-    ValueNode.text = str(visual_inputs[action_number])
+    ValueNode.text = str(visual_inputs[action_number - 1])
 
 func _on_RunEnvironment_set_curve():
     $Path.show()
     $Path.set_curve(curve)
     
 func _on_InputOutput_start_input_visual_entered():
-    ValueNode.text = str(visual_inputs[0])
+    ValueNode.text = start_input
