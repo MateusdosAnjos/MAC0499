6dcd168a7552dff8e6f5b4e9cbebb79ab619d0af
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-09-07 11:45:37 -0300

    Comentarios em Visual Process

--- Phoenix Rising/VisualProcess/VisualProcess.gd
+++ Phoenix Rising/VisualProcess/VisualProcess.gd
@@ -9,11 +9,14 @@ onready var ValueNode = get_node("Path/PathFollow2D/Value")
 
 func _ready():
     $Path.hide()
-    
+
+#Sets the first point of the path  
 func _on_InputOutput_start_input_position(pos):
     starting_pos = (Vector2(pos[0], pos[1]-50))
     start_input = get_parent().INPUT
 
+#Creates the curve, using the positions of the action spaces,
+#that visual process will follow (the offsets are used to make it better to view)
 func _on_RunEnvironment_visual_process_arguments(path_points, intermediate_inputs):
     visual_inputs = intermediate_inputs
     curve.clear_points()
@@ -32,13 +35,17 @@ func _on_RunEnvironment_visual_process_arguments(path_points, intermediate_input
         point[1] += 50
         curve.add_point(point)
 
+#Changes the text on Value, based on the output of the given
+#Action space
 func _on_MovableActionSpace_change_area_entered(action_number):
     ValueNode.text = str(visual_inputs[action_number - 1])
 
+#Sets the start value of the process
+func _on_InputOutput_start_input_visual_entered():
+    ValueNode.text = start_input
+    
+#Sets the curve to follow and starts it
 func _on_RunEnvironment_set_curve():
     $Path.set_curve(curve)
     $Path/PathFollow2D.unit_offset = 0
     $Path.show()
-    
-func _on_InputOutput_start_input_visual_entered():
-    ValueNode.text = start_input
