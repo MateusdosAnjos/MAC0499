7bf1cb46d4fa47c7d90879b68014a4bfaa0b5ad0
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-09-10 19:16:46 -0300

    Posicionado itens para concluir o nível 1

--- Phoenix Rising/Level1/Level1.gd
+++ Phoenix Rising/Level1/Level1.gd
@@ -7,7 +7,7 @@ signal input_output_defined(input, output)
 
 #List of items to be picked up (write one name for each position
 #it will be picked up 1 item for each name position)
-var pickup_item_list = ["print"]
+var pickup_item_list = ["soma", "print"]
 
 #Input and Output of the level, replace it to customize you own level
 const INPUT = '-'
