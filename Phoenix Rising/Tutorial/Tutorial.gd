455554e8fc25508d1213f0bcbed8d3da770f03b0
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-09-11 16:49:18 -0300

    Comando print aceita variável

--- Phoenix Rising/Tutorial/Tutorial.gd
+++ Phoenix Rising/Tutorial/Tutorial.gd
@@ -6,7 +6,7 @@ const INPUT = 'Conquiste o Tutorial'
 const OUTPUT = 'Conquiste o Tutorial'
 
 #List of items to be picked up
-var pickup_item_list = ["print", "A", "B"]
+var pickup_item_list = ["print"]
 
 func _ready():
     var input_text = get_node("InputOutput/InputBase/Input")
