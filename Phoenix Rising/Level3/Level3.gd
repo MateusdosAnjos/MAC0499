f422ae6acbb0ae8bfc53ef2a9205883e2ca3bb5e
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-09-11 20:42:10 -0300

    Botão de próximo nível começa escondido em todos os niveis

--- Phoenix Rising/Level3/Level3.gd
+++ Phoenix Rising/Level3/Level3.gd
@@ -4,7 +4,7 @@ signal input_output_defined(input, output)
 
 #List of items to be picked up (write one name for each position
 #it will be picked up 1 item for each name position)
-var pickup_item_list = ["multi", "print", "A", "B", "soma", "subtracao", "if/else"]
+var pickup_item_list = ["multi", "print"]
 
 #Input and Output of the level, replace it to customize you own level
 const INPUT = '10'
@@ -15,13 +15,16 @@ func _ready():
     input_text.newline()
     input_text.add_text(String(INPUT))
     emit_signal("input_output_defined", INPUT, OUTPUT)
-    #Uncomment line 16 if you want to freeze the game until messages of Users Guide terminates
-    #or until it sets the get_tree().paused = false
+    $NextLevel.hide()
     get_tree().paused = true
 
 func _on_FullScreen_pressed():
     OS.window_fullscreen = !OS.window_fullscreen
 
+#Shows the next level button when the level is succeded
+func _on_RunEnvironment_level_succeded():
+    $NextLevel.show()
+    
 func _on_NextLevel_next_level():
     get_tree().change_scene("res://Level4/Level4.tscn")
 
