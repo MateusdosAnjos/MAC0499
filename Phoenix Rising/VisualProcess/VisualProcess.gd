faafd7880d92fe4afc49dfd13f0bf4cc0f6a8dc5
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-09-13 17:48:59 -0300

    Adicionado loop para trocar o texto do input (falta sincronizar)

--- Phoenix Rising/VisualProcess/VisualProcess.gd
+++ Phoenix Rising/VisualProcess/VisualProcess.gd
@@ -6,6 +6,8 @@ var start_input = ''
 var curve = Curve2D.new()
 var next_value = 0
 var total_inputs = 0
+var current_start_input = 0
+var total_visuals = 0
 
 onready var ValueNode = get_node("Path/PathFollow2D/Value")
 
@@ -16,12 +18,13 @@ func _ready():
 func _on_InputOutput_start_input_position(pos):
     starting_pos = (Vector2(pos[0], pos[1]-50))
     start_input = get_parent().INPUT
-
+    total_inputs = len(start_input)
+    
 #Creates the curve, using the positions of the action spaces,
 #that visual process will follow (the offsets are used to make it better to view)
 func _on_RunEnvironment_visual_process_arguments(path_points, intermediate_inputs):
     visual_inputs = intermediate_inputs
-    total_inputs = len(visual_inputs)
+    total_visuals = len(visual_inputs)
     curve.clear_points()
     $Path.set_curve(curve)
     var curve_points = []
@@ -42,11 +45,12 @@ func _on_RunEnvironment_visual_process_arguments(path_points, intermediate_input
 #Action space
 func _on_MovableActionSpace_change_area_entered():
     ValueNode.text = str(visual_inputs[next_value])
-    next_value = (next_value + 1) % total_inputs
+    next_value = (next_value + 1) % total_visuals
     
 #Sets the start value of the process
 func _on_InputOutput_start_input_visual_entered():
-    ValueNode.text = str(start_input)
+    ValueNode.text = str(start_input[current_start_input])
+    current_start_input = (current_start_input + 1) % total_inputs
     
 #Sets the curve to follow and starts it
 func _on_RunEnvironment_set_curve():
