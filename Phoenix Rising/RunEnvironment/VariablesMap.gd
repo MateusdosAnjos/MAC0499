429faeab9ef10b78d2732450c07c7c9cfea724fa
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-09-17 21:22:42 -0300

    Separado variável A de B no box

--- Phoenix Rising/RunEnvironment/VariablesMap.gd
+++ Phoenix Rising/RunEnvironment/VariablesMap.gd
@@ -1,7 +1,12 @@
 extends Control
 
-onready var VariablesText = get_node("VariableMapBox/VariablesText")
+onready var VariablesTextA = get_node("VariableMapBox/VariablesTextA")
+onready var VariablesTextB = get_node("VariableMapBox/VariablesTextB")
 
 func _on_variable_changed(variable, value):
-    VariablesText.text = str("Variável ", variable, " = ", value)
-    
+    match variable:
+        "A":
+            VariablesTextA.text = str("Variável ", variable, " = ", value)
+        "B":
+            VariablesTextB.text = str("Variável ", variable, " = ", value)
+            
