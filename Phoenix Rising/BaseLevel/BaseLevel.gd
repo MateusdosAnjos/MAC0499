e92172de6bce4fdcbe3ab89908e4d33eeae85e94
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-09-21 15:46:38 -0300

    adicionado botao de retorno ao base level

--- Phoenix Rising/BaseLevel/BaseLevel.gd
+++ Phoenix Rising/BaseLevel/BaseLevel.gd
@@ -28,6 +28,10 @@ func _ready():
 func _on_FullScreen_pressed():
     OS.window_fullscreen = !OS.window_fullscreen
 
+#Returns to title screen
+func _on_ReturnToTitle_pressed():
+    get_tree().change_scene("res://TitleScreen/TitleScreen.tscn")
+    
 #Shows the next level button when the level is succeded
 func _on_RunEnvironment_level_succeded():
     $NextLevel.show()
@@ -37,3 +41,5 @@ func _on_NextLevel_next_level():
 
 func _on_ResetLevel_reset_level():
     get_tree().change_scene("res://BaseLevel/BaseLevel.tscn")
+
+
