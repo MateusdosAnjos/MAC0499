c0cb1b82d5072fc7266fc106bdc5524a94bf98af
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-09-21 15:48:56 -0300

    adicionado botão de retorno em todos os niveis

--- Phoenix Rising/Level1/Level1.gd
+++ Phoenix Rising/Level1/Level1.gd
@@ -22,6 +22,10 @@ func _ready():
 func _on_FullScreen_pressed():
     OS.window_fullscreen = !OS.window_fullscreen
 
+#Returns to title screen
+func _on_ReturnToTitle_pressed():
+    get_tree().change_scene("res://TitleScreen/TitleScreen.tscn")
+
 #Shows the next level button when the level is succeded
 func _on_RunEnvironment_level_succeded():
     $NextLevel.show()
