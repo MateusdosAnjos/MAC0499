caef73bb89bfba09d524b21973a6d59224dd94a5
MateusdosAnjos <mateus.a.anjos@gmail.com>
2019-09-23 14:54:57 -0300

    deletado codigo comentado

--- Phoenix Rising/Behaviors/print.gd
+++ Phoenix Rising/Behaviors/print.gd
@@ -5,10 +5,8 @@ var variable_dict = get_parent().variable_dict
 func execute(input, arguments, action_number):
     if (not arguments.empty()):
         if (arguments == 'input'):
-            #player_answer.append(input)
             get_parent().set_answer_on_screen(str(input) + " ")
         elif (arguments in variable_dict):
-            #player_answer.append(variable_dict[arguments])
             get_parent().set_answer_on_screen(str(variable_dict[arguments]) + " ")
         else:
             return $ErrorMessages.show_error_message("print", action_number)   
